4a1a5fe6c2bc031e9193a52591f9ee305f3efa13
Szkrishna <krisnas9792@gmail.com>
2025-06-25 12:40:09 +0530

    created layout and dashboard component and used layout for this application and added some styles to styles.css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent),
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      { path: '**', 
+        redirectTo: '' 
+      }
+    ]
+  }
+];
+
+
+// import { Routes } from '@angular/router';
+
+// export const routes: Routes = [
+//   {
+//     path: '',
+//     loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent)
+//   }
+// ];
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  selector: 'app-root',
-  template: `<router-outlet></router-outlet>`
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
 })
-export class AppComponent {}
+export class LayoutComponent {}
