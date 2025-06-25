20a818feb52a1cd21af5da68b337c9e340f510ee
Szkrishna <krisnas9792@gmail.com>
2025-06-25 15:25:38 +0530

    topbar completion

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LucideAngularModule, Search, Settings2, CloudDownload } from 'lucide-angular';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  readonly Search = Search;
+  readonly Settings2 = Settings2;
+  readonly CloudDownload = CloudDownload;
+}
